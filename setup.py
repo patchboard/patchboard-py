e89f3714dc7ad4e0a448524066c586576eeb721e
Matthew King <automatthew@gmail.com>
2014-06-02 12:53:35 -0500

    initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='patchboard',
+      version='0.1.0',
+      description='Python client for Patchboard APIs',
+      url='http://github.com/patchboard/patchboard-py',
+      author='Dustin Laurence',
+      author_email='dustin@pandastrike.com',
+      license='MIT',
+      packages=['patchboard'],
+      install_requires=[
+      ],
+      zip_safe=False)
+
