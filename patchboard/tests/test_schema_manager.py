07265c46d08f0fa350647939e3f6b04ccab52ddd
Dustin Laurence <dustin@laurences.net>
2014-06-04 13:24:17 -0700

    Add name_index and media_type index tests

--- patchboard/tests/test_schema_manager.py
+++ patchboard/tests/test_schema_manager.py
@@ -9,14 +9,40 @@ import json
 from patchboard import discover
 
 
-def test_ids():
-    with open(u"patchboard/tests/schema_id_index.json", u"r") as file:
-        correct_schema_id_index = json.load(file)
+def test_id_index():
+    with open(u"patchboard/tests/data/schema_id_index.json", u"r") as file:
+        correct_id_index = json.load(file)
 
     pb = discover(u"patchboard/api.json")
     schema_manager = pb.schema_manager
-    schema_id_index = schema_manager.id_index
+    id_index = schema_manager.id_index
 
     # FIXME: someday need to verify the claim that this properly
     # recurses through the data structures to test for equality
-    assert correct_schema_id_index == schema_id_index
+    assert correct_id_index == id_index
+
+
+def test_name_index():
+    with open(u"patchboard/tests/data/schema_name_index.json", u"r") as file:
+        correct_name_index = json.load(file)
+
+    pb = discover(u"patchboard/api.json")
+    schema_manager = pb.schema_manager
+    name_index = schema_manager.name_index
+
+    # FIXME: someday need to verify the claim that this properly
+    # recurses through the data structures to test for equality
+    assert correct_name_index == name_index
+
+
+def test_media_type_index():
+    with open(u"patchboard/tests/data/schema_media_type_index.json", u"r") as file:
+        correct_media_type_index = json.load(file)
+
+    pb = discover(u"patchboard/api.json")
+    schema_manager = pb.schema_manager
+    media_type_index = schema_manager.media_type_index
+
+    # FIXME: someday need to verify the claim that this properly
+    # recurses through the data structures to test for equality
+    assert correct_media_type_index == media_type_index
