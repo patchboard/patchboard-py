0e27fdd44da43f1f7b796ff84eb54338008a5e20
Dustin Laurence <dustin@laurences.net>
2014-06-04 17:05:19 -0700

    Finish schema_manager tests

--- patchboard/tests/test_schema_manager.py
+++ patchboard/tests/test_schema_manager.py
@@ -1,48 +1,60 @@
-#test_schema_ids.py
+#test_schema_manager.py
 #
 # Copyright 2014 BitVault.
 
+from __future__ import print_function
 
 import json
-
+import pytest
 
 from patchboard import discover
 
 
-def test_id_index():
+@pytest.fixture()
+def pb():
+    return discover(u"patchboard/api.json")
+
+
+@pytest.fixture()
+def schema_manager(pb):
+    return pb.schema_manager
+
+
+def test_id_index(schema_manager):
     with open(u"patchboard/tests/data/schema_id_index.json", u"r") as file:
         correct_id_index = json.load(file)
 
-    pb = discover(u"patchboard/api.json")
-    schema_manager = pb.schema_manager
+    assert correct_id_index == schema_manager.id_index
+
+
+def test_structure_equality(schema_manager):
+    with open(u"patchboard/tests/data/schema_id_index.json", u"r") as file:
+        correct_id_index = json.load(file)
+
+    # Create an inequality deep in the structure to verify that '=='
+    # recurses through structures when checking for equality
     id_index = schema_manager.id_index
+    id_index[u'#account'][u'extends'][u'$ref'] = "foo"
 
-    # FIXME: someday need to verify the claim that this properly
-    # recurses through the data structures to test for equality
-    assert correct_id_index == id_index
+    assert correct_id_index != id_index
 
 
-def test_name_index():
+def test_name_index(schema_manager):
     with open(u"patchboard/tests/data/schema_name_index.json", u"r") as file:
         correct_name_index = json.load(file)
 
-    pb = discover(u"patchboard/api.json")
-    schema_manager = pb.schema_manager
-    name_index = schema_manager.name_index
-
-    # FIXME: someday need to verify the claim that this properly
-    # recurses through the data structures to test for equality
-    assert correct_name_index == name_index
+    assert correct_name_index == schema_manager.name_index
 
 
-def test_media_type_index():
+def test_media_type_index(schema_manager):
     with open(u"patchboard/tests/data/schema_media_type_index.json", u"r") as file:
         correct_media_type_index = json.load(file)
 
-    pb = discover(u"patchboard/api.json")
-    schema_manager = pb.schema_manager
-    media_type_index = schema_manager.media_type_index
+    assert correct_media_type_index == schema_manager.media_type_index
 
-    # FIXME: someday need to verify the claim that this properly
-    # recurses through the data structures to test for equality
-    assert correct_media_type_index == media_type_index
+
+def test_find(schema_manager):
+    # This is almost pointless, given that the find functions are
+    # trivial accessors
+    with pytest.raises(KeyError):
+        schema_manager.find_name(u'not a schema name')
