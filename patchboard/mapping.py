9ef95d8be513a6eb28276216f24309b057be8084
Dustin Laurence <dustin@laurences.net>
2014-06-05 22:49:58 -0700

    Implement Mapping.generate_urls and tests

--- patchboard/mapping.py
+++ patchboard/mapping.py
@@ -5,7 +5,9 @@
 
 from __future__ import print_function
 
-from patchboard.exception import PatchboardError
+from urllib import quote_plus
+
+from exception import PatchboardError
 
 
 class Mapping(object):
@@ -35,5 +37,39 @@ class Mapping(object):
                 u"Mapping is missing any form of URL specification")
 
     def generate_url(self, params={}):
-        # Implement!
-        pass
+        if self.url:
+            base = self.url
+        elif u'url' in params:
+            base = params[u'url']
+        elif self.path:
+            if self.api.service_url:
+                base = "{0}/{1}".format(self.api.service_url, self.path)
+            else:
+                raise PatchboardError(
+                    u"Tried to generate url from path, but API did not define service_url")
+        elif self.template:
+            raise PatchboardError(
+                u"Template mappings are not yet implemented in the client")
+
+        if self.query:
+            parts = []
+            keys = self.query.keys()
+            keys.sort()
+            # TODO check query schema
+            for key in keys:
+                string = params.get(key, None)
+                if string:
+                    parts.append(
+                        u"{0}={1}".format(
+                            quote_plus(key),
+                            quote_plus(string)))
+
+            if len(parts) > 0:
+                query_string = "?{0}".format(parts.join(u'&'))
+            else:
+                query_string = ""
+
+            return base + query_string
+
+        else:
+            return base
---
+++ patchboard/tests/unit_tests/test_mapping.py
@@ -0,0 +1,28 @@
+#test_mapping.py
+#
+# Copyright 2014 BitVault.
+
+from __future__ import print_function
+
+import json
+import pytest
+
+
+from patchboard.exception import PatchboardError
+from patchboard.tests.fixtures import pb, api
+pytest.mark.usefixtures(pb, api)
+
+
+def test_generate_url(api):
+    with open(u"patchboard/tests/data/api_mappings_urls.json", u"r") as file:
+        ruby_urls = json.load(file)
+
+    python_urls = {}
+    for key, mapping in api.mappings.iteritems():
+        try:
+            #print(len(mapping.generate_url()))
+            python_urls[key] = mapping.generate_url()
+        except PatchboardError as e:
+            python_urls[key] = "Exception: '{0}'".format(e)
+
+    assert ruby_urls == python_urls
