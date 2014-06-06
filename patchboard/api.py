c2338e9777a4f64d70f10d4838e694a0bf8a9362
Dustin Laurence <dustin@laurences.net>
2014-06-05 19:01:49 -0700

    Finish api class

--- patchboard/api.py
+++ patchboard/api.py
@@ -39,6 +39,44 @@ class API(object):
         else:
             return None
 
-    def decorate(context, schema, data):
-        # TODO: implement!
-        pass
+    def decorate(self, context, schema, data):
+        if not schema:
+            return data
+
+        mapping = self.find_mapping(schema)
+        if mapping:
+            # when we have a resource class, instantiate it using the
+            # input data.
+            data = mapping.cls(context, data)
+        else:
+            # Otherwise traverse the schema in search of subschemas
+            # that have resource classes available.
+            schema_type = schema[u'type']
+            if schema_type == u'array':
+                # TODO: handle the case where schema.items is an array,
+                # which signifies a tuple.  schema.additionalItems
+                # then becomes important.
+                data = [self.decorate(context, schema[u'items'], item)
+                        for item in data]
+            elif schema_type == u'object':
+                if u'properties' in schema:
+                    for key, prop_schema in schema[u'properties']:
+                        value = data.get(key, None)
+                        if value:
+                            data[key] = self.decorate(
+                                context,
+                                prop_schema,
+                                value)
+
+                # TODO: handle schema.patternProperties
+                # TODO: consider alternative to iterating over all keys.
+                if u'additionalProperties' in schema:
+                    for key, value in data:
+                        if (u'properties' not in schema or
+                                key not in schema[u'properties']):
+                            data[key] = self.decorate(
+                                context,
+                                schema[u'additionalProperties'],
+                                value)
+
+        return data
