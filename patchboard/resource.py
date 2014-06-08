d442caa65dedaafe09a659fccf4689a4bbb3b248
Dustin Laurence <dustin@laurences.net>
2014-06-08 11:52:28 -0700

    Fill out resources more

--- patchboard/resource.py
+++ patchboard/resource.py
@@ -20,9 +20,9 @@ class ResourceType(type):
     def __init__(cls, name, patchboard, definition, schema, mapping):
         super(ResourceType, cls).__init__(name, (Resource,), {})
 
-        # TODO: add the singleton methods in Resource::assemble()
+        setattr(cls, 'api', classmethod(lambda(self_): patchboard.api))
 
-        #setattr(cls, 'api', lambda(self): patchboard.api)
+        setattr(cls, 'schema', classmethod(lambda(self_): mapping))
 
         if schema:
             if u'properties' in schema:
@@ -33,12 +33,20 @@ class ResourceType(type):
                         lambda(self): self.attributes[name])
 
             if schema.get(u'additionalProperties', False) is not False:
-                # FIXME: doesn't take the additional args in the ruby
-                # code.
-                setattr(
-                    cls,
-                    'method_missing',
-                    lambda(self, name): self.attributes[name])
+                # FIXME: doesn't take the block the ruby code does
+                def fn(self, name, *args):
+                    if len(args) == 0:
+                        return self.attributes[name]
+                    else:
+                        return super(cls, self).method_missing(name, *args)
+
+                setattr(cls, 'method_missing', fn)
+
+        setattr(
+            cls,
+            'generate_url',
+            classmethod(
+                lambda(self_, params): mapping.generate_url(params)))
 
         for name, action in definition[u'actions'].iteritems():
             # FIXME: create actions
@@ -55,12 +63,33 @@ class Resource(object):
         # TODO: non destructive decoration
         # TODO: add some sort of validation for the input attributes.
 
-        if self.schema and u'properties' in self.schema:
+        if cls.schema and u'properties' in cls.schema:
             context = instance.context
-            properties = self.schema[u'properties']
+            properties = cls.schema[u'properties']
             for key, sub_schema in properties.iteritems():
-                if key in attributes:
-                    value = attributes[key]
+
+                if key not in attributes:
+                    next
+
+                value = attributes[key]
+
+                mapping = cls.api.find_mapping(sub_schema)
+                if mapping:
+                    if mapping.query:
+                        # TODO: find a way to define this at runtime,
+                        # not once for every instance.
+                        def fn(self, params={}):
+                            params[u'url'] = value[u'url']
+                            url = mapping.generate_url(params)
+                            return mapping.cls(context, {u'url': url})
+                        setattr(instance, key, fn)
+                    else:
+                        attributes[key] = mapping.cls(context, value)
+                else:
+                    attributes[key] = cls.api.decorate(
+                        context,
+                        sub_schema,
+                        value)
 
         return attributes
 
