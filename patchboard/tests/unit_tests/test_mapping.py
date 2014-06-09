87058faa9904a5b4a01fe3955fd84130bbe39bde
Dustin Laurence <dustin@laurences.net>
2014-06-08 17:50:16 -0700

    Update test_mapping.py

--- patchboard/tests/unit_tests/test_mapping.py
+++ patchboard/tests/unit_tests/test_mapping.py
@@ -9,16 +9,17 @@ import pytest
 
 
 from patchboard.exception import PatchboardError
-from patchboard.tests.fixtures import mock_pb, mock_api
-pytest.mark.usefixtures(mock_pb, mock_api)
+from patchboard.tests.fixtures import (mock_pb, net_pb, mock_api, net_api,
+                                       api)
+pytest.mark.usefixtures(mock_pb, net_pb, mock_api, net_api, api)
 
 
-def test_generate_url(mock_api):
+def test_generate_url(api):
     with open(u"patchboard/tests/data/api_mappings_urls.json", u"r") as file:
         ruby_urls = json.load(file)
 
     python_urls = {}
-    for key, mapping in mock_api.mappings.iteritems():
+    for key, mapping in api.mappings.iteritems():
         try:
             #print(len(mapping.generate_url()))
             python_urls[key] = mapping.generate_url()
