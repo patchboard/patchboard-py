bd3becdad7b38925ee2a174613b15290ebebc3d8
Dustin Laurence <dustin@laurences.net>
2014-06-08 17:46:42 -0700

    Update test_api.py

--- patchboard/tests/unit_tests/test_api.py
+++ patchboard/tests/unit_tests/test_api.py
@@ -8,29 +8,30 @@ import json
 import pytest
 
 
-from patchboard.tests.fixtures import mock_pb, mock_api
-pytest.mark.usefixtures(mock_pb, mock_api)
+from patchboard.tests.fixtures import (mock_pb, net_pb, mock_api, net_api,
+                                       api)
+pytest.mark.usefixtures(mock_pb, net_pb, mock_api, net_api, api)
 
 
-def test_service_url(mock_api):
-    assert mock_api.service_url is None
+def test_service_url(api):
+    assert api.service_url is None
 
 
-def test_resources(mock_api):
+def test_resources(api):
     with open(u"patchboard/tests/data/api_resources.json", u"r") as file:
         ruby_resources = json.load(file)
 
-    assert ruby_resources == mock_api.resources
+    assert ruby_resources == api.resources
 
 
-def test_schemas(mock_api):
+def test_schemas(api):
     with open(u"patchboard/tests/data/api_schemas.json", u"r") as file:
         ruby_schemas = json.load(file)
 
-    assert ruby_schemas == mock_api.schemas
+    assert ruby_schemas == api.schemas
 
 
-def test_mappings(mock_api):
+def test_mappings(api):
     with open(u"patchboard/tests/data/api_mappings.json", u"r") as file:
         ruby_mappings = json.load(file)
 
@@ -38,12 +39,12 @@ def test_mappings(mock_api):
     ruby_keys = ruby_mappings.keys()
     ruby_keys.sort()
 
-    python_keys = mock_api.mappings.keys()
+    python_keys = api.mappings.keys()
     python_keys.sort()
 
     assert ruby_keys == python_keys
 
 
-def test_find_mapping(mock_api):
+def test_find_mapping(api):
     # TODO: implement!
     pass
