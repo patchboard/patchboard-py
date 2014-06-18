d7875128fffab255e4f6eb16ebf4f5dd86ebd9c3
Dustin Laurence <dustin@laurences.net>
2014-06-18 09:51:35 -0700

    Remove xfail decorator

--- patchboard/tests/functional/trivial_example_test.py
+++ patchboard/tests/functional/trivial_example_test.py
@@ -19,7 +19,7 @@ pytest.mark.usefixtures(trivial_net_pb,
                         trivial_net_users)
 
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_users_create(trivial_net_users):
     user = trivial_net_users.create(
         {u'login': "foo-{0}".format(randint(1,100000))})
