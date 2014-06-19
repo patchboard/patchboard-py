ad8f54f7a2532b0ad643790d061b4e488ac7849d
Dustin Laurence <dustin@laurences.net>
2014-06-18 21:11:50 -0700

    Remove xfail from working test

--- patchboard/tests/functional/trivial_example_test.py
+++ patchboard/tests/functional/trivial_example_test.py
@@ -19,7 +19,6 @@ pytest.mark.usefixtures(trivial_net_pb,
                         trivial_net_users)
 
 
-@pytest.mark.xfail
 def test_users_create(trivial_net_users):
     login = "foo-{0}".format(randint(1, 100000))
     user = trivial_net_users.create({u'login': login})
