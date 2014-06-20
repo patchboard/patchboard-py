8f230c5419e9eb1534bb608235a14377ce177702
Dustin Laurence <dustin@laurences.net>
2014-06-20 07:29:23 -0700

    Add more tests to play.py

--- patchboard/tests/scripts/play.py
+++ patchboard/tests/scripts/play.py
@@ -16,20 +16,14 @@ users = resources.users
 login = "foo-{0}".format(randint(1, 100000))
 user = users.create({u'login': login})
 
-# FIXME:
-# Problem 1 (probably the root problem): user.questions() only  takes
-# 1 argument (in Python self counts, so it's a zero-argument method by most
-# people's count).
 questions = user.questions({u'category': u'Science'})
-# It's a pain to track down calls to dynamically created methods, but
-# the function being called seems to be the one constructed at
-# resource.py:37. I suspect it's not the correct function getting called,
-# since this all works in the ruby client.
+print("questions type:", type(questions))
 
-## This works, but it's kind of pointless to look at what it does
-## if it isn't the right call in the first place.
-#questions = user.questions()  # ({u'category': u'Science'})
-## Doing this, you get back a dict instead of a Questions object, but
-## I suspect this is simply a symptom of the above problem
-#print("Questions:", questions)
-#print("Questions type:", type(questions))
+question = questions.ask()
+print("question type:", type(question))
+
+# FIXME: this call fails
+result = question.answer({u'letter': u'd'})
+#print("result type:", type(result))
+#print(result.correct)
+#print(result.success)
