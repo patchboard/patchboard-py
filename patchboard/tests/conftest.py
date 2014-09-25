520e34bb8f177b3d7a0ec65290cdbb796a901298
Matt Smith <matt.the.smith@gmail.com>
2014-09-25 20:39:28 +0000

    added explicit --port option to py.test and set default to 1979 instead of 80

--- patchboard/tests/conftest.py
+++ patchboard/tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--host", action="store", default='localhost', help="specify the url of test host (scheme is optional and defaults to http, port is optional and defaults to 80)")
+    parser.addoption("--host", action="store", default='localhost', help="specify the url of test host (optional) default: localhost (scheme is optional and defaults to http)")
+    parser.addoption("--port", action="store", default='1979', help="specify the port of test host (optional) default: 1979")
 
 @pytest.fixture(scope=u'session')
 def api_url(request):
-    url = request.config.getoption('--host')
+    url = "{}:{}".format(request.config.getoption('--host'),
+                         request.config.getoption('--port'))
     return u'http://' + url if u'://' not in url else url
