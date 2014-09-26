87f3c155014cf5eadc7f5d7441116b1df2ccb097
Matt Smith <matt.the.smith@gmail.com>
2014-09-26 18:53:43 +0000

    integrated pytest with setuptools; pull down patchboard-examples and run trivial sample server if no --host argument is provided

--- patchboard/tests/scripts/play.py
+++ patchboard/tests/scripts/play.py
@@ -1,25 +1,62 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env /usr/bin/python
 
-
 from __future__ import print_function
 
-from random import randint
+import subprocess
+import tarfile, os
 
+from glob import glob
+from time import sleep
+from urllib import urlretrieve
+from random import randint
 from patchboard import discover
 
 
-pb = discover(u"http://localhost:1979/")
-resources = pb.resources
-users = resources.users
+# This repo contains a coffeescript patchboard server.
+try:
+    example_dir = glob("automatthew*")
+    if not example_dir:
+        if not os.path.exists(u'./pb-ex.tar.gz'):
+            retrieving
+            urlretrieve(u'http://github.com/automatthew/patchboard-examples/tarball/master',
+                        u'./pb-ex.tar.gz')
+        with tarfile.open(u'./pb-ex.tar.gz', u'r:gz') as tar:
+            for tarmember in tar.getnames():
+                if u'trivial' in tarmember:
+                    trivial_dir = trivial_dir if u'trivial_dir' in locals() else tarmember
+                    tar.extract(tarmember)
+    else:
+        trivial_dir = "{}/{}/".format(example_dir[0], u'trivial')
 
-login = "foo-{0}".format(randint(1, 100000))
-user = users.create({u'login': login})
+    trivial_dir = os.getcwd() + '/' + trivial_dir
 
-questions = user.questions({u'category': u'Science'})
-print("questions type:", type(questions))
+    subprocess.Popen(u'npm install', cwd=trivial_dir, shell=True).wait()
+    server = subprocess.Popen(u'bin/server.coffee test/data/questions.json', cwd=trivial_dir, shell=True)
 
-question = questions.ask()
-print("question type:", type(question))
+    # Uuuuugh, need a better way to delay until the server is listening that doesn't wait for the process to terminate.
+    sleep(2)
 
-result = question.answer({u'letter': u'd'})
+    pb = discover(u'http://localhost:1979/')
+    resources = pb.resources
+    users = resources.users
+
+    login = "foo-{0}".format(randint(1, 100000))
+    user = users.create({u'login': login})
+
+    questions = user.questions({u'category': u'Science'})
+    print("questions type:", type(questions))
+
+    question = questions.ask()
+    print("question type:", type(question))
+
+    result = question.answer({u'letter': u'd'})
+
+    # TODO Need to terminate this process after the test suite runs, or we'll get a port error.
+    # server.terminate()
+
+except Exception as e:
+    if server:
+        server.kill()
+    print(e)
+    SystemExit(1)
--- setup.py
+++ setup.py
@@ -1,17 +1,45 @@
 # -*- coding: utf-8 -*-
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run(self):
+        errno = 0
+        if u"host" not in self.pytest_args:
+            import sys,subprocess
+            errno = subprocess.call([sys.executable, "patchboard/tests/scripts/play.py"])
+            if errno > 0:
+                raise SystemExit(errno)
+        import pytest
+        errno = max(errno, pytest.main(self.pytest_args))
+        raise SystemExit(errno)
 
 setup(name='patchboard',
-      version='0.1.0',
+      version='0.1.1',
       description='Python client for Patchboard APIs',
       url='http://github.com/patchboard/patchboard-py',
       author='Dustin Laurence',
       author_email='dustin@pandastrike.com',
       license='MIT',
       packages=find_packages(exclude=[
-          u'*.tests', u'*.tests.*', u'tests.*', u'tests']),
-      install_requires=[
-          'pytest', 'requests',
-      ],
-      zip_safe=False)
-
+          u'*.tests', u'*.tests.*', u'tests.*', u'tests',
+      ]),
+      install_requires=['requests'],
+      tests_require=[
+        'pytest',
+        ],
+      cmdclass = {'test': PyTest},
+      zip_safe=False
+      )
